d3da6b6711d96e16acb06c611ed9ff080ed7ffc2
AlvinSun <gangyongsun@163.com>
2019-10-18 08:43:49 +0800

    密码md5加密问题

--- fileUploadDemo/src/main/resources/static/js/system/login.js
+++ fileUploadDemo/src/main/resources/static/js/system/login.js
@@ -1,97 +1,97 @@
 jQuery(document).ready(
-		function() {
-			try {
-				var _href = window.location.href + "";
-				if (_href && _href.indexOf('?kickout') != -1) {
-					layer.msg('您已经被踢出，请重新登录！');
-				}
-			} catch (e) {
-				console.log(e);
-			}
+function() {
+	try {
+		var _href = window.location.href + "";
+		if (_href && _href.indexOf('?kickout') != -1) {
+			layer.msg('您已经被踢出，请重新登录！');
+		}
+	} catch (e) {
+		console.log(e);
+	}
 
-			/**
-			 * 回车事件绑定
-			 */
-			document.onkeydown = function(event) {
-				var e = event || window.event
-						|| arguments.callee.caller.arguments[0];
-				if (e && e.keyCode == 13) {
-					$('#login').click();
-				}
-			};
+	/**
+	 * 回车事件绑定
+	 */
+	document.onkeydown = function(event) {
+		var e = event || window.event
+				|| arguments.callee.caller.arguments[0];
+		if (e && e.keyCode == 13) {
+			$('#login').click();
+		}
+	};
 
-			/**
-			 * 登录操作
-			 */
-			$('#login').click(function() {
-				var username = $('#username').val();
-				var password = $('#password').val();
-				if (username == '') {
-					$('.error').fadeOut('fast', function() {
-						$('.error').css('top', '27px').show();
-					});
-					$('.error').fadeIn('fast', function() {
-						$('.username').focus();
-					});
-					return false;
-				}
-				if (password == '') {
-					$('.error').fadeOut('fast', function() {
-						$('.error').css('top', '96px').show();
-					});
-					$(this).find('.error').fadeIn('fast', function() {
-						$('.password').focus();
-					});
-					return false;
-				}
-				// var password = MD5(username + "#" + password);
-				var data = {
-					userName : username,
-					passWord : password,
-					rememberMe : $("#rememberMe").is(':checked')
-				};
-				console.log(data);
-				var load = layer.load();
+	/**
+	 * 登录操作
+	 */
+	$('#login').click(function() {
+		var username = $('#username').val();
+		var password = $('#password').val();
+		if (username == '') {
+			$('.error').fadeOut('fast', function() {
+				$('.error').css('top', '27px').show();
+			});
+			$('.error').fadeIn('fast', function() {
+				$('.username').focus();
+			});
+			return false;
+		}
+		if (password == '') {
+			$('.error').fadeOut('fast', function() {
+				$('.error').css('top', '96px').show();
+			});
+			$(this).find('.error').fadeIn('fast', function() {
+				$('.password').focus();
+			});
+			return false;
+		}
+		var password = MD5(username + "#" + password);
+		var data = {
+			userName : username,
+			passWord : password,
+			rememberMe : $("#rememberMe").is(':checked')
+		};
+		console.log(data);
+		var load = layer.load();
 
-				$.ajax({
-					url : "submitLogin",
-					data : data,
-					type : "post",
-					dataType : "json",
-					beforeSend : function() {
-						layer.msg('开始登录，请注意后台控制台!');
-					},
-					success : function(result) {
-						if (result) {
-							layer.msg(result.message);
-							if (result.status != 200) {
-								$('.password').val('');
-								return;
-							} else {
-								setTimeout(function() {
-									// 登录返回
-									window.location.href = result.back_url;
-								}, 1000)
-							}
-						}
-						layer.close(load);
-					},
-					error : function(e) {
-						console.log(e, e.message);
-						layer.msg('请看后台Java控制台，是否报错，确定已经配置数据库和Redis');
-						layer.close(load);
+		$.ajax({
+			url : "submitLogin",
+			data : data,
+			type : "post",
+			dataType : "json",
+			beforeSend : function() {
+				layer.msg('开始登录，请注意后台控制台!');
+			},
+			success : function(result) {
+				if (result) {
+					layer.msg(result.message);
+					if (result.status != 200) {
+						$('.password').val('');
+						return;
+					} else {
+						setTimeout(function() {
+							// 登录返回
+							window.location.href = result.back_url;
+						}, 1000)
 					}
-				});
-			});
-			$('.page-container form .username, .page-container form .password')
-					.keyup(function() {
-						$(this).parent().find('.error').fadeOut('fast');
-					});
-
-			/**
-			 * 注册跳转
-			 */
-			$("#register").click(function() {
-				window.location.href = "register";
-			});
+				}
+				layer.close(load);
+			},
+			error : function(e) {
+				console.log(e, e.message);
+				layer.msg('请看后台Java控制台，是否报错，确定已经配置数据库和Redis');
+				layer.close(load);
+			}
 		});
+	});
+	$('.page-container form .username, .page-container form .password')
+			.keyup(function() {
+				$(this).parent().find('.error').fadeOut('fast');
+			});
+
+	/**
+	 * 注册跳转
+	 */
+	$("#register").click(function() {
+		window.location.href = "register";
+	});
+});
