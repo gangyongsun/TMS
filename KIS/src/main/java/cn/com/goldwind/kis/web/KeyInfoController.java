73ef577ea99835e765763508170b669dfbb70658
AlvinSun <gangyongsun@163.com>
2019-10-23 15:56:16 +0800

    术语详情告一段落

--- KIS/src/main/java/cn/com/goldwind/kis/web/KeyInfoController.java
+++ KIS/src/main/java/cn/com/goldwind/kis/web/KeyInfoController.java
@@ -1,5 +1,6 @@
 package cn.com.goldwind.kis.web;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,7 +31,7 @@ public class KeyInfoController {
 	 */
 	@RequestMapping(value = "index")
 	public ModelAndView index(ModelMap map, String termType, String findContent) {
-		System.out.println("findContent="+findContent);
+		System.out.println("findContent=" + findContent);
 		// 查询所有术语类型
 		List<String> keyInfoTypeList = keyInfoService.findTermTypes();
 
@@ -38,11 +39,11 @@ public class KeyInfoController {
 		List<KeyInfo> keyInfoList = null;
 		if (null != findContent && !"".equalsIgnoreCase(findContent)) {
 			keyInfoList = keyInfoService.findByKeyInfo(findContent);
-		}else {
+		} else {
 			if (null != termType || !"".equalsIgnoreCase(termType)) {
 				// 按术语类型搜索关键词
 				keyInfoList = keyInfoService.findByTermType(termType);
-			} 
+			}
 		}
 
 		if (null != keyInfoTypeList && keyInfoTypeList.size() > 0) {
@@ -67,7 +68,55 @@ public class KeyInfoController {
 		if (null != keyInfo) {
 			map.put("keyInfo", keyInfo);
 		}
+		List<String> sentenceList = getSentenceArrayList(keyInfo);
+		if (null != sentenceList && sentenceList.size() > 0) {
+			map.put("sentenceList", sentenceList);
+		}
+
 		return new ModelAndView("detail");
+	}
+
+	/**
+	 * 解析例句和例句来源
+	 * 
+	 * @param keyInfo
+	 * @return
+	 */
+	private List<String> getSentenceArrayList(KeyInfo keyInfo) {
+		List<String> sentenceList = new ArrayList<String>();
+
+		String[] sentenceCNArray = keyInfo.getSentenceCN().split("\\$\\$");
+		String[] sentenceENArray = keyInfo.getSentenceEN().split("\\$\\$");
+		String[] sentenceCNResourceArray = keyInfo.getSentenceCNResource().split("\\$\\$");
+		String[] sentenceENResourceArray = keyInfo.getSentenceENResource().split("\\$\\$");
+
+		int size = sentenceCNArray.length > sentenceENArray.length ? sentenceCNArray.length : sentenceENArray.length;
+
+		String sentenceCN = "";
+		String sentenceCNResource = "";
+		String sentenceEN = "";
+		String sentenceENResource = "";
+		for (int i = 0; i < size; i++) {
+			sentenceCN = sentenceCNArray[i];
+			sentenceCNResource = sentenceCNResourceArray[i];
+			sentenceEN = sentenceENArray[i];
+			sentenceENResource = sentenceENResourceArray[i];
+
+			if (null != sentenceCNResource && !"".equals(sentenceCNResource)) {
+				sentenceCN += "【来源: " + sentenceCNResource + "】";
+			}
+			if (null != sentenceENResource && !"".equals(sentenceENResource)) {
+				sentenceEN += "【SOURCE: " + sentenceENResource + "】";
+			}
+
+			if (null != sentenceCN && !"".equals(sentenceCN)) {
+				sentenceList.add(sentenceCN);
+			}
+			if (null != sentenceEN && !"".equals(sentenceEN)) {
+				sentenceList.add(sentenceEN);
+			}
+		}
+		return sentenceList;
 	}
 
 	/**
